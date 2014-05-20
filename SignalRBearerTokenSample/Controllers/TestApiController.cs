adc0583cd2396f07f8a46ee6e7111b681d68b2b3
Marcin Budny <marcin.budny@gmail.com>
2014-05-20 19:59:49 +0200

    added an example of a Web API call

---
+++ SignalRBearerTokenSample/Controllers/TestApiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SignalRBearerTokenSample.Controllers
+{
+    public class TestApiController : ApiController
+    {
+        [HttpGet]
+        [Route("api/whoami")]
+        public string GetUserName()
+        {
+            if (User.Identity.IsAuthenticated)
+                return User.Identity.Name;
+
+            return "anonymous";
+        }
+    }
+}
